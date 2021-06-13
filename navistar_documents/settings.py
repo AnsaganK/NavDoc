352435357dc99781bb76b370389c5d8aa7590d53
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-06-13 14:11:26 +0600

    17 commit

--- navistar_documents/settings.py
+++ navistar_documents/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-from constants import WKHTMLTOPDF_CMD_PATH
+from constants import WKHTMLTOPDF_CMD_PATH, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -87,17 +87,16 @@ WSGI_APPLICATION = 'navistar_documents.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
 
 
 
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -108,7 +107,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
