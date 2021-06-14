40faab1eed61161f0abe738e7cbfd2486cfd43b4
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-06-14 07:23:03 +0600

    17 commit

--- navistar_documents/settings.py
+++ navistar_documents/settings.py
@@ -155,5 +155,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-WKHTMLTOPDF_CMD = WKHTMLTOPDF_CMD_PATH
+WKHTMLTOPDF_CMD = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
 #WKHTMLTOPDF_CMD = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
